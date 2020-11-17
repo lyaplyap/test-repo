5c599ed3dde4389966e418cbc589cc264f992838
lyaplyap <64465746+lyaplyap@users.noreply.github.com>
2020-11-17 10:23:07 +0300

    Updates

---
+++ precache-manifest.dd502abf910e50fa126008716fe30470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aea5e0312f71eef94c8ccd2c23d4e0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4edeb0548d99aed416d4",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "4edeb0548d99aed416d4",
+    "url": "./static/js/2.a45d65a3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.a45d65a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7932ac695be2edf20f65",
+    "url": "./static/js/main.43db896b.chunk.js"
+  },
+  {
+    "revision": "31d64bb6ba6eb870fc9b",
+    "url": "./static/js/runtime-main.4e9d023e.js"
+  }
+]);
